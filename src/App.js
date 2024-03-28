7f47e61b4a277a9bb9e0953d984e911328bb1af9
Aidan Lao <laoaidan@gmail.com>
2024-03-28 12:32:42 -0300

    Added homepage clouds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+
 import './App.css';
 
 function App() {
   return (
-    <div className="">
-      Kai-bot
+    <div className="homepage">
+      <div className="homepageContent">
+        <h1>My name is Kai! How can I help you today?</h1>
+      </div>
     </div>
   );
 }
