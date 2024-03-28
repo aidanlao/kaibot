76a440f73a22c96a7fafd248fb038a96928670ad
Aidan Lao <laoaidan@gmail.com>
2024-03-28 14:39:54 -0300

    Added open ai and submit button for symptoms

--- src/views/symptoms.js
+++ src/views/symptoms.js
@@ -2,12 +2,18 @@ import { useState } from "react";
 
 export default function Symptoms() {
     const [symptoms, setSymptoms] = useState();
+    function handleSubmit() {
+        
+    }
     return (
       <>
         <div className="symptoms">
             <h1>Describe your symptoms:</h1>
+            <form onSubmit={(e)=> {handleSubmit(e)}}>
             <textarea value={symptoms} onChange={(e)=> {setSymptoms(e.target.value);}}></textarea>
-        </div>  
+            <input type="submit" className="submit" value="Submit"></input>
+            </form>
+           </div>  
       </>  
     );
 }
