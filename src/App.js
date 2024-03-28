9438449e718841103a46ff628ec894a07bc8602b
Aidan Lao <laoaidan@gmail.com>
2024-03-28 13:27:41 -0300

    added call nurse page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Welcome from './views/welcome';
 import  { useState } from 'react';
 import useSound from 'use-sound';
 import click from './click.mp3';
+import CallNurse from './views/callNurse';
 function App() {
   const [play] = useSound(click);
   const [page, setPage] = useState(0);
@@ -15,12 +16,14 @@ function App() {
 
   const viewList = [
     <Welcome next={next}/>,
-    <Purpose next={next}/>
+    <Purpose next={next}/>,
+    <CallNurse/>,
   ]
 
   return (
     <>
     <div className="restart"><a onClick={()=>{setPage(0)}}href="#">Restart</a></div>
+    <div className="callNurse"><a onClick={()=>{setPage(2)}}>Call Nurse</a></div>
     {viewList[page]}
     </>
   );
---
+++ src/views/callNurse.js
@@ -0,0 +1,15 @@
+export default function CallNurse() {
+    return (
+        <>
+
+
+            <div className="page nurse">
+                <div className="pageContent">
+                    <i class="fa-solid fa-phone fa-2xl"></i>
+                    <h1>Calling nurse... hold on tight!</h1>
+                </div>
+            </div>
+        </>
+    );
+
+}
