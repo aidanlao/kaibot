1051264de4c60b1bba575cf11e5fe34ca8541c79
Aidan Lao <laoaidan@gmail.com>
2024-03-28 12:50:03 -0300

    Added buttons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ function App() {
   return (
     <div className="homepage">
       <div className="homepageContent">
-        <h1>My name is Kai! How can I help you today?</h1>
+        <h1>Welcome, My name is Kai! <br></br> Who is this visit for?</h1>
+        <div className="buttons">
+          <div className="button">
+          <i class="fa-solid fa-user fa-2xl"></i>
+          <h2>Myself</h2>
+          </div>
+          <div className="button">
+          <i class="fa-solid fa-children fa-2xl"></i>
+          <h2>Dependent</h2>
+          </div>
+          <div className="button">
+          <i class="fa-solid fa-arrow-right fa-2xl"></i>
+          <h2>Other</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
